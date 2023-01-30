c4e44d99179e9880aea91d3ce465f2c79ad85247
mariorenanofc <mariorenan25@gmail.com>
2023-01-30 18:33:51 -0300

    student: V-show
    Entendendo a diferença entre V-show, v-if e quando usa-los!

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+
+    }
+</script>
+<template>
+    <header> 
+        Sessão de importação do template
+    </header>
+</template>
+
+<style scoped>
+    
+    header {   
+        font-size: 20px;
+        font-weight: 800;
+        padding: 10px 0;
+        color: aqua;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #222;
+    }
+</style>
